7e2c26e7d89d62e62a0caad7a3188a9f1c16f45b
wopian <wopian@wopian.me>
2023-02-14 18:07:02 +0000

    test: add test stump

--- src/cli.ts
+++ src/cli.ts
@@ -6,7 +6,7 @@ import { combine } from './index.js'
 
 program.option('-i, --input <input>', 'input folder path containing CSV files')
 
-program.option('-o, --output <output>', 'output folder path to save JSON files' )
+program.option('-o, --output <output>', 'output folder path to save JSON files')
 
 program.parse()
 
